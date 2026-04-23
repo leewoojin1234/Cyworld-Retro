1d1af10faf04cbc58714ac28994257fef7e4dfe6
leewoojin1234 <geonui542@gmail.com>
2026-04-23 16:05:58 +0900

    feat :: 기본 세팅 추가

---
+++ src/main/java/kr/hs/dgsw/cyworldretro/CyworldRetroApplication.java
@@ -0,0 +1,13 @@
+package kr.hs.dgsw.cyworldretro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyworldRetroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CyworldRetroApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/hs/dgsw/cyworldretro/CyworldRetroApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.hs.dgsw.cyworldretro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CyworldRetroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
