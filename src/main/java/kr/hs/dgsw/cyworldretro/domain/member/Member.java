62cd1bee379292fde164a1d2a7878aac024f64c9
leewoojin1234 <geonui542@gmail.com>
2026-05-07 15:12:05 +0900

    feat :: 멤버 엔티티 추가

---
+++ src/main/java/kr/hs/dgsw/cyworldretro/domain/member/Member.java
@@ -0,0 +1,40 @@
+package kr.hs.dgsw.cyworldretro.domain.member;
+
+import jakarta.persistence.*;
+import kr.hs.dgsw.cyworldretro.global.entity.BaseTimeEntity;
+import lombok.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class Member extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String nickname;
+
+    @Builder.Default
+    private int acorn = 0;
+
+    public void addAcorn(int amount) {
+        this.acorn += amount;
+    }
+
+    public void useAcorn(int amount) {
+        if (this.acorn < amount) {
+            throw new RuntimeException("도토리가 부족합니다.");
+        }
+        this.acorn -= amount;
+    }
+}
