203ed8b7563cdb5f42c770095f23f223703a0a4c
leewoojin1234 <geonui542@gmail.com>
2026-05-07 16:02:36 +0900

    feat :: 방명록 기능 추가

---
+++ src/main/java/kr/hs/dgsw/cyworldretro/domain/guestbook/GuestBook.java
@@ -0,0 +1,36 @@
+package kr.hs.dgsw.cyworldretro.domain.guestbook;
+
+import jakarta.persistence.*;
+import kr.hs.dgsw.cyworldretro.domain.member.Member;
+import kr.hs.dgsw.cyworldretro.domain.minihome.MiniHome;
+import kr.hs.dgsw.cyworldretro.global.entity.BaseTimeEntity;
+import lombok.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class GuestBook extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "minihome_id")
+    private MiniHome miniHome;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "writer_id")
+    private Member writer;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    private boolean isSecret;
+
+    public void updateContent(String content) {
+        this.content = content;
+    }
+}
---
+++ src/main/java/kr/hs/dgsw/cyworldretro/domain/guestbook/GuestBookRepository.java
@@ -0,0 +1,10 @@
+package kr.hs.dgsw.cyworldretro.domain.guestbook;
+
+import kr.hs.dgsw.cyworldretro.domain.minihome.MiniHome;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GuestBookRepository extends JpaRepository<GuestBook, Long> {
+    Page<GuestBook> findAllByMiniHome(MiniHome miniHome, Pageable pageable);
+}
